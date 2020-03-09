99b15d8643fb00dfcdabc2e8a663a447fb3d06fa
Orlane Vermo <orlane_v@hotmail.com>
2020-03-09 15:27:27 +0100

    Comit 1 - TransferObjects Done

---
+++ Permackathon.Common/Enums/Priority.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Permackathon.Common.Enums
+{
+    public enum Priority
+    {
+        Low,
+        Medium,
+        High
+    }
+}
