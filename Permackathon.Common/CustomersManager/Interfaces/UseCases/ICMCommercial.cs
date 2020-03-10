3cf456051189e5ca366ec55ed7529a4189fdbdcf
patrick-peycker <patrick.peycker@outlook.com>
2020-03-10 17:10:11 +0100

    BE20200310 (3)

---
+++ Permackathon.Common/CustomersManager/Interfaces/UseCases/ICMCommercial.cs
@@ -0,0 +1,17 @@
+﻿using Permackathon.Common.CustomersManager.TransferObject;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Permackathon.Common.CustomersManager.Interfaces.UseCases
+{
+	public interface ICMCommercial : ICMUser
+	{
+		// Create
+		CustomerTO AddCustomer(CustomerTO Customer);
+		// Update
+		CustomerTO UpdateCustomer(CustomerTO Customer);
+		// Delete - byId
+		void DeleteCustomer(int Id);
+	}
+}
